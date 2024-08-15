b42666e82e5a8dce24abae41c9b36d778eeeb6dd
Shyamraj V S <shyamrajvs737@gmail.com>
2024-08-15 20:31:58 +0530

    third commit

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,8 +2,25 @@ import axios from 'axios';
 
 const url ='https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+    id : number ;
+    title : string ;
+    completed : boolean;
+}
+
 axios.get(url).then(response => {
 
-    console.log(response.data);
+    const todo = response.data as Todo;
+
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    console.log(`
+        The todo with ID : ${id}
+        Has a title of : ${title}
+        Is it finished ? ${completed}
+        `);
+    
 
 });
